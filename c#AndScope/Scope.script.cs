f70b884766695e1e12da14117c7537c5b752940b
Xiangli Wu <2445282010@qq.com>
2018-04-25 21:56:38 +0800

    c#andScope_combine

---
+++ c#AndScope/Scope.script.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.SCOPE.Types;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ScopeRuntime;
+
+public static class MyHelper
+{
+    public static string SecondsToMinutes(string str)
+    {
+        string tmp = "wokaohhhhhh";
+        return tmp;
+    }
+}
+
+
